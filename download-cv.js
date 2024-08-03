0a9ebe1fe4b9d70adf8607138be39158ba08dff8
namanjeetyadav <157890838+namanjeetyadav@users.noreply.github.com>
2024-08-03 23:11:33 +0530

    Created download-cv.js

---
+++ download-cv.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadLink = document.getElementById('download-cv');
+
+    downloadLink.addEventListener('click', function() {
+        alert('Your download will start shortly.');
+    });
+});
